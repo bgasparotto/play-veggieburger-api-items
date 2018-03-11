55b8d0f83fd94970fd1f250e8088d28da04f204e
bgasparotto <bruno.m.gasparotto@gmail.com>
2018-03-11 18:25:31 -0300

    Finished implementing the first version of the REST API.

--- app/controllers/ItemController.java
+++ app/controllers/ItemController.java
@@ -1,10 +1,76 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import models.model.Item;
+import models.persistence.ItemRepository;
+import play.libs.Json;
 import play.mvc.*;
 
-public class ItemController extends Controller {
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
 
-    public Result view() {
-        return ok("addresses");
+/**
+ * REST API controller for the Item resource.
+ *
+ * @author Bruno Gasparotto
+ */
+public class ItemController extends Controller {
+    private ItemRepository repository;
+
+    @Inject
+    public ItemController(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    public Result view(Long id) {
+        Item item = repository.findOne(id);
+        Optional<Item> o = Optional.ofNullable(item);
+
+        return o.map(i -> ok(Json.toJson(i))).orElseGet(Results::notFound);
+    }
+
+    public Result list() {
+        List<Item> items = repository.findAll();
+        return ok(Json.toJson(items));
+    }
+
+    public Result create() {
+        JsonNode json = request().body().asJson();
+        Item item = Json.fromJson(json, Item.class);
+
+        Long id = repository.insert(item);
+        return created(Json.toJson(id));
+    }
+
+    public Result update(Long id) {
+        JsonNode json = request().body().asJson();
+        Item item = Json.fromJson(json, Item.class);
+        item.setId(id);
+
+        Item updated = repository.update(item);
+        return ok(Json.toJson(updated));
+    }
+
+    public Result modify(Long id) {
+        JsonNode json = request().body().asJson();
+        Item item = Json.fromJson(json, Item.class);
+        Item existing = repository.findOne(id);
+
+        Optional.ofNullable(item.getName()).ifPresent(s -> existing.setName(s));
+        Optional.ofNullable(item.getPrice()).ifPresent(p -> existing.setPrice(p));
+
+        Item updated = repository.update(existing);
+        return ok(Json.toJson(updated));
+    }
+
+    public Result remove(Long id) {
+        repository.delete(id);
+        return ok();
+    }
+
+    public Result removeAll() {
+        repository.clear();
+        return ok();
     }
 }
