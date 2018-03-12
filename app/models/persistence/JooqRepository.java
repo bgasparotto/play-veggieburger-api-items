08077314a22a322638641fe08b7fd16fe07e6a44
bgasparotto <bruno.m.gasparotto@gmail.com>
2018-03-12 17:24:42 -0300

    Updated the JOOQ repository to use the provider instead of managing the connection by itself.

--- app/models/persistence/JooqRepository.java
+++ app/models/persistence/JooqRepository.java
@@ -1,5 +1,7 @@
 package models.persistence;
 
+import models.persistence.jooq.PlayConnectionProvider;
+import org.jooq.ConnectionProvider;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
@@ -33,8 +35,8 @@ public abstract class JooqRepository {
      * @return A initialised {@code DSLContexts}
      */
     protected DSLContext create() {
-        Connection connection = database.getConnection();
-        DSLContext dslContext = DSL.using(connection, SQLDialect.H2);
+        ConnectionProvider provider = new PlayConnectionProvider(database);
+        DSLContext dslContext = DSL.using(provider, SQLDialect.H2);
         return dslContext;
     }
 }
