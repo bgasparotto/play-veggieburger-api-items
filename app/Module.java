23271243130b3e778a5da14fed335c1241057247
bgasparotto <bruno.m.gasparotto@gmail.com>
2018-03-11 23:14:32 -0300

    Added javadoc.

--- app/Module.java
+++ app/Module.java
@@ -2,6 +2,12 @@ import com.google.inject.AbstractModule;
 import models.persistence.CollectionItemRepository;
 import models.persistence.ItemRepository;
 
+/**
+ * Module which declares the bindings between interfaces and their respective
+ * implementations.
+ *
+ * @author Bruno Gasparotto
+ */
 public class Module extends AbstractModule {
 
     @Override
