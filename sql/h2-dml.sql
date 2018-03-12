581cbd8adc57e2acbbcbafaba3191a6c36f85ba2
bgasparotto <bruno.m.gasparotto@gmail.com>
2018-03-12 14:20:30 -0300

    Added the SQL for initialising the H2 database.

---
+++ sql/h2-dml.sql
@@ -0,0 +1,8 @@
+INSERT INTO item
+(ID, NAME, PRICE)
+VALUES
+(1, 'Original Veggie Burger', 10),
+(2, 'Small Veggie Burger', 9),
+(3, 'Medium Veggie Burger', 11),
+(4, 'Big Veggie Burger', 13),
+(5, 'Family Veggie Burger', 18);
