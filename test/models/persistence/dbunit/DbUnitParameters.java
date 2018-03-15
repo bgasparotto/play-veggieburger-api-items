4bc984fb5823eb69362a88f1b276aa833685bffd
bgasparotto <bruno.m.gasparotto@gmail.com>
2018-03-15 08:12:05 -0300

    Added an enum containing DbUnit constants for running tests.

---
+++ test/models/persistence/dbunit/DbUnitParameters.java
@@ -0,0 +1,62 @@
+package models.persistence.dbunit;
+
+/**
+ * {@code DbUnit} constants for database connection.
+ * 
+ * @author Bruno Gasparotto
+ *
+ */
+public enum DbUnitParameters {
+
+	/**
+	 * Driver class.
+	 */
+	DRIVER_CLASS("org.h2.Driver"),
+
+	/**
+	 * Connection URL of the database.
+	 */
+	CONNECTION_URL("jdbc:h2:mem:play;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;INIT=runscript from 'sql/h2-ddl.sql'"),
+
+	/**
+	 * Username to be used to access the database.
+	 * */
+	USERNAME("sa"),
+
+	/**
+	 * Password used by the {@link DbUnitParameters#USERNAME USERNAME}.
+	 */
+	PASSWORD(""),
+
+	/**
+	 * Path of the {@code xml} file containing the data to be used on persitence
+	 * tests by {@code DbUnit}.
+	 */
+	FLAX_XML_FILE("test/dbunit-test-db.xml"),
+
+	/**
+	 * The persistence unit to be used on test context.
+	 */
+	PERSISTENCE_UNIT("archproject-local");
+
+	private final String value;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param value
+	 *            The constant value
+	 */
+	private DbUnitParameters(String value) {
+		this.value = value;
+	}
+
+	/**
+	 * The the value of this enum element.
+	 * 
+	 * @return Value of this enum element
+	 */
+	public String getValue() {
+		return value;
+	}
+}
