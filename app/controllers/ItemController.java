acafee046454b3f7385e1f3eceac5368e63edb27
bgasparotto <bruno.m.gasparotto@gmail.com>
2018-03-23 09:37:24 -0300

    Currently updating the Item controller for processing the requests asynchronously. (It's not compiling yet, I had to commit and run!).

--- app/controllers/ItemController.java
+++ app/controllers/ItemController.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import models.model.Item;
 import models.persistence.ItemRepository;
 import play.libs.Json;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
 
 import javax.inject.Inject;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 /**
  * REST API controller for the Item resource.
@@ -20,10 +23,14 @@ import java.util.Optional;
  */
 public class ItemController extends Controller {
     private ItemRepository repository;
+    private HttpExecutionContext executionContext;
 
     @Inject
-    public ItemController(ItemRepository repository) {
+    public ItemController(ItemRepository repository,
+                          HttpExecutionContext executionContext) {
+
         this.repository = repository;
+        this.executionContext = executionContext;
     }
 
     /**
@@ -32,11 +39,12 @@ public class ItemController extends Controller {
      * @param id The {@code id} of the resource to be viewed
      * @return The resource corresponding to the given {@code id}
      */
-    public Result view(Long id) {
-        Item item = repository.findOne(id);
-        Optional<Item> o = Optional.ofNullable(item);
-
-        return o.map(i -> ok(Json.toJson(i))).orElseGet(Results::notFound);
+    public CompletionStage<Result> view(Long id) {
+        return repository.findOne(id)
+                .thenApplyAsync(itemOptional -> itemOptional
+                                .map(i -> ok(Json.toJson(i)))
+                                .orElseGet(Results::notFound),
+                        executionContext.current());
     }
 
     /**
@@ -45,9 +53,10 @@ public class ItemController extends Controller {
      * @return The collection of all the available resources, or an empty
      * collection if no resources exist
      */
-    public Result list() {
-        List<Item> items = repository.findAll();
-        return ok(Json.toJson(items));
+    public CompletionStage<Result> list() {
+        return repository.findAll()
+                .thenApplyAsync(items -> ok(Json.toJson(items)),
+                        executionContext.current());
     }
 
     /**
@@ -55,17 +64,20 @@ public class ItemController extends Controller {
      *
      * @return The created resource
      */
-    public Result create() {
+    public CompletionStage<Result> create() {
         JsonNode json = request().body().asJson();
         Item item = Json.fromJson(json, Item.class);
 
         if (Objects.nonNull(item.getId())) {
-            String message = "Resouce id must not be provided.";
-            return badRequest(Json.toJson(message));
+            return supplyAsync(() -> {
+                String message = "Resouce id must not be provided.";
+                return badRequest(Json.toJson(message));
+            }, executionContext.current());
         }
 
-        Item createdItem = repository.insert(item);
-        return created(Json.toJson(createdItem));
+        return repository.insert(item)
+                .thenApplyAsync(createdItem -> created(Json.toJson(createdItem)),
+                        executionContext.current());
     }
 
     /**
@@ -74,14 +86,15 @@ public class ItemController extends Controller {
      * @param id The {@code id} of the resource to be updated
      * @return The updated resource
      */
-    public Result update(Long id) {
+    public CompletionStage<Result> update(Long id) {
         JsonNode json = request().body().asJson();
         Item item = Json.fromJson(json, Item.class);
         item.setId(id);
 
-        Item updatedItem = repository.update(item);
-        Optional<Item> o = Optional.ofNullable(updatedItem);
-        return o.map(i -> ok(Json.toJson(i))).orElseGet(Results::notFound);
+        return repository.update(item).thenApplyAsync(updatedItem -> {
+            Optional<Item> o = Optional.ofNullable(updatedItem);
+            return o.map(i -> ok(Json.toJson(i))).orElseGet(Results::notFound);
+        }, executionContext.current());
     }
 
     /**
@@ -90,20 +103,19 @@ public class ItemController extends Controller {
      * @param id The {@code id} of the resource to be modified
      * @return The modified resource
      */
-    public Result modify(Long id) {
+    public CompletionStage<Result> modify(Long id) {
         JsonNode json = request().body().asJson();
         Item item = Json.fromJson(json, Item.class);
-        Item existing = repository.findOne(id);
 
-        if (existing == null) {
-            return notFound();
-        }
+        return repository.findOne(id).thenApplyAsync(itemOptional -> {
+            itemOptional.map((existing) -> {
+                Optional.ofNullable(item.getName()).ifPresent(existing::setName);
+                Optional.ofNullable(item.getPrice()).ifPresent(existing::setPrice);
 
-        Optional.ofNullable(item.getName()).ifPresent(existing::setName);
-        Optional.ofNullable(item.getPrice()).ifPresent(existing::setPrice);
-
-        Item updatedItem = repository.update(existing);
-        return ok(Json.toJson(updatedItem));
+                return repository.update(existing)
+                        .thenApplyAsync(updatedItem -> ok(Json.toJson(updatedItem)));
+            }).orElseGet(supplyAsync(() -> Results::notFound));
+        });
     }
 
     /**
@@ -112,7 +124,7 @@ public class ItemController extends Controller {
      * @param id The {@code id} of the resource to be removed
      * @return The {@code OK} HTTP status code
      */
-    public Result remove(Long id) {
+    public CompletionStage<Result> remove(Long id) {
         int deletedResources = repository.delete(id);
         if (deletedResources == 0) {
             return notFound();
@@ -126,8 +138,9 @@ public class ItemController extends Controller {
      *
      * @return The {@code OK} HTTP status code
      */
-    public Result removeAll() {
-        repository.deleteAll();
-        return ok();
+    public CompletionStage<Result> removeAll() {
+        return repository.deleteAll()
+                .thenApplyAsync((i) -> ok(),
+                        executionContext.current());
     }
 }
